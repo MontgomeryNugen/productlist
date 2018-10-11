205cace11f590ae4222b77f192206038acae9596
MontgomeryNugen <montgomery.nugen@gmail.com>
2018-10-11 13:40:20 -0400

    minor changes

--- js/productslist.js
+++ js/productslist.js
@@ -1,5 +1,27 @@
 $().ready(function () {
 
+    var url = "http://localhost:8080/Products/List";
 
-    
+    $.getJSON(url)
+        .then(function(products) {
+            console.log(products);
+            render(products);
+        });   
 });
+
+function render(products) {
+    var tbody = $("#tbody");
+    // var products = products;   
+    tbody.empty();
+    for(var product of products) {
+        var row = "<tr>";
+        row += "<td>" + product.id + "</td>";
+        row += "<td>" + product.name + "</td>";
+        row += "<td>" + product.price + "</td>";
+        row += "<td>" + product.vendor.name + "</td>";
+        row += "</tr>";
+
+        tbody.append(row);
+    }
+
+}
